636c36d471d9004221fcbab2e37663dfd34f88d0
shuibo <sbjyemail@gmail.com>
2017-03-22 17:02:13 +0800

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'topics#index'
-  resources :topics
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :topics do
+    member do
+      post 'upvote'
+    end
